d424d0658953b2ba4e5ad12b09a979fe10546da6
dbarelop <dbarelop@gmail.com>
2016-12-18 11:24:35 +0100

    Adapted MAAS deploy and release scripts to take more than one hostname as a parameter

--- maas/scripts/maas_deploy.sh
+++ maas/scripts/maas_deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-test ! -z $1 || { echo "USAGE: $0 <hostname>"; exit 1; }
-hostname=$1
-echo -n "Getting system_id for $hostname... "
-system_id=$(maas $USER machines read | jq -r "map(select(.hostname == \"$hostname\")) | .[] | .system_id")
-test ! -z $system_id && echo "$system_id" || { echo "error"; exit 1; }
-echo -n "Allocating machine... "
-maas $USER machines allocate system_id=$system_id > /dev/null && echo "success" || { echo "error"; exit 1; }
-echo -n "Performing deployment... "
-maas $USER machine deploy $system_id > /dev/null && echo "success" || { echo "error"; exit 1; }
+test ! -z $1 || { echo "USAGE: $0 [hostname ...]"; exit 1; }
+while (( $# )); do
+    hostname=$1
+    shift
+    echo -n "Getting system_id for $hostname... "
+    system_id=$(maas $USER machines read | jq -r "map(select(.hostname == \"$hostname\")) | .[] | .system_id")
+    test ! -z $system_id && echo "$system_id" || { echo "error"; continue; }
+    echo -n "Allocating machine... "
+    maas $USER machines allocate system_id=$system_id > /dev/null && echo "success" || { echo "error"; continue; }
+    echo -n "Performing deployment... "
+    maas $USER machine deploy $system_id > /dev/null && echo "success" || { echo "error"; continue; }
+done
--- maas/scripts/maas_release.sh
+++ maas/scripts/maas_release.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-test ! -z $1 || { echo "USAGE: $0 <hostname>"; exit 1; }
-hostname=$1
-echo -n "Getting system_id for $hostname... "
-system_id=$(maas $USER machines read | jq -r "map(select(.hostname == \"$hostname\")) | .[] | .system_id")
-test ! -z $system_id && echo "$system_id" || { echo "error"; exit 1; }
-echo -n "Releasing machine... "
-maas dbarelop machine release $system_id > /dev/null && echo "success" || { echo "error"; exit 1; }
+test ! -z $1 || { echo "USAGE: $0 [hostname ...]"; exit 1; }
+while (( $# )); do
+    hostname=$1
+    shift
+    echo -n "Getting system_id for $hostname... "
+    system_id=$(maas $USER machines read | jq -r "map(select(.hostname == \"$hostname\")) | .[] | .system_id")
+    test ! -z $system_id && echo "$system_id" || { echo "error"; continue; }
+    echo -n "Releasing machine... "
+    maas dbarelop machine release $system_id > /dev/null && echo "success" || { echo "error"; continue; }
+done
