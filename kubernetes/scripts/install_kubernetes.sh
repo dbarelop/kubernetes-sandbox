2e4c24ccaee6d419be1f5dd64bfcb074fff8b58c
Daniel Barea <dbarelop@gmail.com>
2017-01-09 14:26:28 +0100

    Replaced apt-get with apt

--- kubernetes/scripts/install_kubernetes.sh
+++ kubernetes/scripts/install_kubernetes.sh
@@ -4,5 +4,5 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+apt update
+apt install -y docker.io kubelet kubeadm kubectl kubernetes-cni
