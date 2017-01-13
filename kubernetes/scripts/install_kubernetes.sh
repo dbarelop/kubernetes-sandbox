0e7b23c942788b048adcb49d2093e52ef41e2b90
dbarelop <dbarelop@gmail.com>
2017-01-13 12:33:51 +0100

    Added support for installing Kubernetes via SSH to a set of hosts

--- kubernetes/scripts/install_kubernetes.sh
+++ kubernetes/scripts/install_kubernetes.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt update
-apt install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+test ! -z $1 || { echo "USAGE: $0 [hostname ...]"; exit 1; }
+
+while (( $# )); do
+    hostname=$1
+    shift
+    ssh -t $hostname "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -"
+    ssh -t $hostname "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list"
+    ssh -t $hostname "sudo apt update"
+    ssh -t $hostname "sudo apt install -y docker.io kubelet kubeadm kubectl kubernetes-cni"
+done
